5c3ce1866dc227a644dbeb56cce739a61d192177
Егор Кучук <kriptyashka@gmail.com>
2025-09-17 21:39:40 +0300

    wip: Добавлен storage

--- cmd/simple_voting/main.go
+++ cmd/simple_voting/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log/slog"
 	"os"
-	"simple-votings/internal/config"
+	"simple_voting/internal/config"
+	"simple_voting/internal/lib/logger/handlers/slogdiscard/sl"
+	"simple_voting/internal/storage/sqlite"
 )
 
 const (
@@ -14,19 +16,33 @@ const (
 
 func main() {
 	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env)) // к каждому сообщению будет добавляться поле с информацией о текущем окружении
+
+	log.Info("initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
+	log.Debug("logger debug mode enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to initialize storage", sl.Err(err))
+	}
 }
 
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 
+	var level slog.Level
+
 	switch env {
 	case envLocal:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		level = slog.LevelDebug
 	case envDev:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		level = slog.LevelDebug
 	case envProd:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+		level = slog.LevelInfo
 	}
+	log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level}))
 
 	return log
 }
